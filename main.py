219cd66c0af4407310f4d2ce17e64c791f052a08
danraponga <danrapongha@gmail.com>
2024-12-29 22:32:16 +0100

    update script: add conn pool, docker-compose

--- main.py
+++ main.py
@@ -22,59 +22,66 @@ async def send_data_to_server(data: dict) -> str:
             return response
     except websockets.exceptions.ConnectionClosedOK:
         pass
+    except Exception as e:
+        logging.error(f"WebSocket error: {e}")
 
 
-async def process_data(conn, cur):
-    await cur.execute(
-        f"""
-        SELECT id, carwash_id, post_id, payment_type, value
-        FROM transactions
-        WHERE payment_type IN ('cash', 'paypass', 'liqpay')
-          AND value > 0
-          AND id NOT IN (SELECT id FROM fiscalized)
-          AND service_id = 14
-          AND last_update > NOW() - INTERVAL {INTERVAL_HOURS} HOUR
-        """
-    )
-    result = await cur.fetchall()
-
-    for row in result:
-        now = datetime.now()
-        factory_number = "ADW" + str(row["carwash_id"]) + str(row["post_id"])
-        
-        response = await send_data_to_server({
-            "factory_number": factory_number,
-            "sales": {
-                "code": row["id"],
-                row["payment_type"]: row["value"],
-                "created_at": now.strftime("%Y-%m-%dT%H:%M:%S")
-            }
-        })
-        logging.info(f"Response for transaction_id={row['id']}: {response}")
-
+async def process_data(pool):
+    async with pool.acquire() as conn, conn.cursor(aiomysql.DictCursor) as cur:
         await cur.execute(
+            f"""
+            SELECT id, carwash_id, post_id, payment_type, value
+            FROM transactions
+            WHERE payment_type IN ('cash', 'paypass', 'liqpay')
+            AND value > 0
+            AND id NOT IN (SELECT id FROM fiscalized)
+            AND service_id = 14
+            AND last_update > NOW() - INTERVAL {INTERVAL_HOURS} HOUR
             """
-            INSERT INTO fiscalized (id, serial, type, value, date, is_handled)
-            VALUES (%s, %s, %s, %s, %s, %s)
-            """,
-            (row["id"], factory_number, row["payment_type"], row["value"], now, True)
         )
-        await conn.commit()
-        await asyncio.sleep(2)
+        result = await cur.fetchall()
+
+        for row in result:
+            now = datetime.now()
+            factory_number = "ADW" + str(row["carwash_id"]) + str(row["post_id"])
+            
+            response = await send_data_to_server({
+                "factory_number": factory_number,
+                "sales": {
+                    "code": row["id"],
+                    row["payment_type"]: row["value"],
+                    "created_at": now.strftime("%Y-%m-%dT%H:%M:%S")
+                }
+            })
+            if response:
+                logging.info(f"Response for transaction_id={row['id']}: {response}")
+
+            await cur.execute(
+                """
+                INSERT INTO fiscalized (id, serial, type, value, date, is_handled)
+                VALUES (%s, %s, %s, %s, %s, %s)
+                """,
+                (row["id"], factory_number, row["payment_type"], row["value"], now, True)
+            )
+            await conn.commit()
 
 
 async def main_loop():
     logging.basicConfig(level=logging.INFO)
-    code = 1
-    async with await aiomysql.connect(
-            host=os.getenv("DB_HOST"),
-            port=int(os.getenv("DB_PORT")),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            db=os.getenv("DB_NAME"),
-    ) as conn:
-        async with conn.cursor(aiomysql.DictCursor) as cur:
-            while True:
-                await process_data(conn, cur)
+    pool = aiomysql.create_pool(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        db=os.getenv("DB_NAME"),
+    )
+    while True:
+        try:
+            await process_data(pool)
+        except Exception as e:
+            logging.error(f"Unexpected error: {e}")
+            
+        await asyncio.sleep(2)
+
 
 asyncio.run(main_loop())
